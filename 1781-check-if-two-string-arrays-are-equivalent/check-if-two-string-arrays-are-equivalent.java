ef21c4218240c30191dd0782e323d107cca5d6a9
tanishjain09 <153806176+tanishjain09@users.noreply.github.com>
2024-05-03 15:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (94.51%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-       StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
 
-        String temp1 = Arrays.toString(word1);
-        String clean_1 = temp1.replace("[", "");
-        String clean_2 = clean_1.replace("]", "");
-        String clean_3 = clean_2.replace(" ", "");
-        String clean_final = clean_3.replace(",", "");
-        sb1.append(clean_final);
+        for(String value: word1){
+            s1.append(value);
+        }
+        for(String value: word2){
+            s2.append(value);
+        }
+        if(s1.toString().equals(s2.toString())){
+            return true;
+        }
+        return false;
 
-        String temp2 = Arrays.toString(word2);
-        String tclean_1 = temp2.replace("[", "");
-        String tclean_2 = tclean_1.replace("]", "");
-        String tclean_3 = tclean_2.replace(" ", "");
-        String tclean_final = tclean_3.replace(",", "");
-        sb2.append(tclean_final);
-
-            if(sb1.toString().equals(sb2.toString())){
-                return true;
-            }
-            return false; 
     }
 }
