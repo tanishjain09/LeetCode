8332761ff3a10af367c97f2d272948516a4bb1cc
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-07-02 19:28:59 +0530

    Time: 2 ms (80.51%) | Memory: 45.8 MB (84.37%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,24 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int left = 0;
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
 
-        for(int right=0;right<n; right++){
-            if(nums[right]!=0){
-                int temp = nums[right];
-                nums[right]=nums[left];
-                nums[left]=temp;
-                left++;
+        //find the first occurence of 0 and store it in j
+        int j = -1;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        //using two pointer check if element at i is 0 or not if not than swap the 0 with ith element and if ith element is zero than just increment i and check for next index
+        if(j > -1){
+            for(int i = j+1; i<n; i++) {
+                if (arr[i] != 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    j++;
+                }
             }
         }
     }
