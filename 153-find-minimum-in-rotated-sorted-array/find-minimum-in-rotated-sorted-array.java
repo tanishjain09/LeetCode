9d26376f23d0a09144ea36080bb36a52a1bf86f8
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-07-07 10:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (87.00%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,9 +1,13 @@
 class Solution {
     public int findMin(int[] arr) {
+
+        //as per analysis the min is always the next index of pivot if arrary is rotated 
+        //and if array is not rotate means pivot will be -1 as we define the pivot method
         int pivot = findPivot(arr);
         return arr[pivot + 1];
     }
 
+    //first find the pivot that is largest in the array index
     private int findPivot(int[] arr){
         int start = 0;
         int end = arr.length - 1;
@@ -23,6 +27,6 @@ class Solution {
                 start = mid + 1;
             }
         }
-        return -1;
+        return -1; // this means that the array is not rotated 
     }
 }
