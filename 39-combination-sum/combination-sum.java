bdf0ea9bd182da16a21c843482ec4ecd783e95be
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-07-14 08:55:16 +0530

    Time: 1 ms (99.91%) | Memory: 44.9 MB (31.33%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        finalCombination(0, candidates,target, new ArrayList<>(),ans);
+        return ans;
+    
+    }
+
+    private void finalCombination(int index, int[] arr ,int target, List<Integer> list, List<List<Integer>> ans){
+        if(index == arr.length){
+            if(target == 0){
+            ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+
+        
+
+        if(arr[index] <= target){
+            list.add(arr[index]);
+            finalCombination(index, arr, target - arr[index], list, ans);
+            list.remove(list.size() - 1);
+        }
+        finalCombination(index + 1, arr, target, list, ans);
+    }
+}
