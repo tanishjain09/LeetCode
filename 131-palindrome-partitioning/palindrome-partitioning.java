349c78dfc27bdd23ec4881a9003af3605d5f062f
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-07-19 11:01:52 +0530

    Time: 8 ms (52.41%) | Memory: 56.9 MB (58.56%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,27 +1,27 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        palindromepartitioning(s,0,ans,new ArrayList<>());
+        palindromeParti(0, s, ans, new ArrayList<String>());
         return ans;
     }
 
-    private void palindromepartitioning(String s, int index, List<List<String>> ans, List<String> list){
+    private void palindromeParti(int index, String s, List<List<String>> ans, List<String> list){
         if(index == s.length()){
             ans.add(new ArrayList<>(list));
             return;
         }
-        
+
         for(int i = index; i < s.length(); i++){
-            if(isPlaindrome(s, index, i)){
+            if(isPalindrome(s, index, i)){
                 list.add(s.substring(index, i+1));
-                palindromepartitioning(s, i+1,ans, list);
+                palindromeParti(i + 1,s, ans, list);
                 list.remove(list.size() - 1);
             }
         }
     }
 
-    private boolean isPlaindrome(String s, int start, int end) {
-        while (start <= end){
+    private boolean isPalindrome(String s, int start, int end){
+        while(start <= end){
             if(s.charAt(start++) != s.charAt(end--)) return false;
         }
         return true;
