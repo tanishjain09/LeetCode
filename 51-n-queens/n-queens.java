22e9409c9a302cbcf53640f590dbd8d1c5bd08c0
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-07-19 10:39:10 +0530

    Time: 2 ms (86.66%) | Memory: 44.7 MB (95.15%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,68 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board= new char[n][n];
+
+        for(int i = 0; i < n; i++){
+            for(int j= 0; j < n; j++){
+                board[i][j] = '.';
+            }
+        }
+        List<List<String>> result =  new ArrayList<>();
+        backtrack(0, board,result);
+        return  result;
+    }
+
+
+    private void backtrack(int col, char[][] board, List<List<String>> result){
+        if(col == board.length){
+            result.add(construct(board));
+            return;
+        }
+
+        for(int row = 0; row < board.length; row++){
+            if(isSafe(board, row, col)){
+                board[row][col] = 'Q';
+                backtrack(col + 1, board, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private List<String> construct(char[][] board){
+        List<String> res = new ArrayList<String>();
+
+        for(int i = 0; i <  board.length; i++){
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+    private boolean isSafe(char[][] board, int row, int col){
+        int dupRow = row;
+        int dupCol = col;
+
+        while(row >= 0 && col >= 0){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = dupRow;
+        col = dupCol;
+
+        while(col >= 0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = dupRow;
+        col = dupCol;
+
+        while(col >= 0 && row < board.length){
+            if(board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+}
