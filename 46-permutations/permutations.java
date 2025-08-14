f603f70e7c75a06c3bbdf21519e9fffefd571f19
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-08-14 13:31:03 +0530

    Time: 1 ms (92.68%) | Memory: 44.5 MB (63.38%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -5,28 +5,26 @@ class Solution {
         allPermu(0, nums, ans);
         return ans;
     }
-
     private void allPermu(int index, int[] arr, List<List<Integer>> ans){
         if(index == arr.length){
             List<Integer> list = new ArrayList<>();
-            for(int i = 0; i < arr.length; i++){
-                list.add(arr[i]);
+            for(int num : arr){
+                list.add(num);
             }
             ans.add(list);
             return;
         }
 
+
         for(int i = index; i < arr.length; i++){
             swap(arr, i, index);
-            allPermu(index + 1, arr, ans);
-            swap(arr, index, i);
+            allPermu(index+1, arr, ans);
+            swap(arr,index, i);
         }
     }
-
     private void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-
 }
