c3f8237fddf31764801044658a309401a6ffe205
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-08-20 13:06:18 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (72.88%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,36 +1,33 @@
 class Solution {
-    public double findMedianSortedArrays(int[] input1, int[] input2) {
-        if(input1.length > input2.length){
-            return findMedianSortedArrays(input2, input1);
-        }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        int x = input1.length;
-        int y = input2.length;
-
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
         int low = 0;
-        int high = x;
+        int high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        int n = n1 + n2;
         while(low <= high){
-            int partitionX = (low + high) / 2;
-            int partitionY = (x + y + 1)/2 - partitionX;
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1; 
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
 
-            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
-            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
-
-            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : input2[partitionY - 1];
-            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
-
-            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
-                if((x+y) % 2 == 0){
-                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
-                }else{
-                    return (double)Math.max(maxLeftX, maxLeftY);
-                }
-            }else if(maxLeftX > minRightY){
-                high = partitionX - 1;
-            }else{
-                low = partitionX + 1;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1-1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 -1];
+            
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return Math.max(l1, l2);
+                return (double)(Math.max(l1, l2) + Math.min(r1,r2)) / 2.0;
             }
+            else if(l1 > l2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-        return 0.0;
+        return 0;
     }
 }
