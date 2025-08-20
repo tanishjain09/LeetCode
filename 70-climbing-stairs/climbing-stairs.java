df5f9aac6cf3d4cfc4b49e27e6a359478e80df8d
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-08-20 15:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (65.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev2 = 1;
-        int prev = 1;
-        for(int i = 2; i <= n; i++){
-            int curr_i = prev + prev2;
-            prev2 = prev;
-            prev = curr_i;
-        }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return climbStairs(n, dp);
+    }
+    private int climbStairs(int n, int[] dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
 
-        return prev;
+        return dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
+        
     }
 }
