c5fac51c39a4201d512a9234edf0c685b26dd91e
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-07 08:06:32 +0530

    Time: 1 ms (99.5%), Space: 57.3 MB (31.51%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+
+        for(int num : nums){
+            sum += num;
+            if(sum > maxi){
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+}
