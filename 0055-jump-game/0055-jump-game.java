5d66cce8b0b2a45538c4e12e3e1d147f3e0b4a5c
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-09 00:10:11 +0530

    Time: 2 ms (83.89%), Space: 45.6 MB (85.31%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxInd = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > maxInd) return false;
+            maxInd = Math.max(maxInd, i + nums[i]);
+            if(maxInd >= nums.length-1) return true;
+        }
+        return true;
+    }
+}
