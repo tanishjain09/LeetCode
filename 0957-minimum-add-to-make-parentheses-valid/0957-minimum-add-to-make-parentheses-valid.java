db8877396e5ab7f93b8ef15c92521cd2466627cd
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-10 21:38:53 +0530

    Time: 0 ms (100%), Space: 41.7 MB (29.17%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open= 0;
+        int add = 0;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                open++;
+            }else{
+                if(open > 0) open--;
+                else add++;
+            }
+        }
+        return open + add;
+    }
+}
