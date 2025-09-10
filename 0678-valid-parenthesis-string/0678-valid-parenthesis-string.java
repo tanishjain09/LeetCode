8be040a060db27bd5767b4c74f19848d69904ac4
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-10 22:14:18 +0530

    Time: 0 ms (100%), Space: 41.3 MB (89.47%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int min = 0, max = 0;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                min++;
+                max++;
+            }else if(c == ')'){
+                min--;
+                max--;
+            }else{
+                min--;
+                max++;
+            }
+            if(min < 0) min = 0;
+            if(max < 0) return false;
+        }
+        return min == 0;
+    }
+}
