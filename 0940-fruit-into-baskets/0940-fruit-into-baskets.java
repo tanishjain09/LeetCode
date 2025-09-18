12abb0617334c99ba849ba73819052314012115e
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-18 10:33:17 +0530

    Time: 51 ms (37.28%), Space: 55.2 MB (61.81%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -4,7 +4,7 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         while(r < fruits.length){
             map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
-            while(map.size() > 2){
+            if(map.size() > 2){
                 map.put(fruits[l], map.getOrDefault( fruits[l], 0) - 1);
                 if(map.get(fruits[l]) == 0) map.remove(fruits[l]);
                 l++;
