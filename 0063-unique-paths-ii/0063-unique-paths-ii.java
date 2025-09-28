479b1180cb26f240b16156e413c21f9e6853a09e
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-28 15:33:23 +0530

    Time: 0 ms (100%), Space: 41.8 MB (66.24%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,18 +3,18 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         int[][] dp = new int[n][m];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j]=1;
+                else{
+                    int up = (i > 0) ? dp[i-1][j] : 0;
+                    int left = (j > 0) ? dp[i][j-1] : 0;
+                    dp[i][j] = up+left;
+                }
+            }
         }
-        return solve(n-1, m-1, grid, dp);
+        return dp[n-1][m-1];
     }
-    private int solve(int n, int m, int[][] grid,int[][] dp){
-        if(n < 0 || m < 0) return 0;
-        if(grid[n][m] == 1) return 0;
-        if(n == 0 && m == 0) return 1;
-        if(dp[n][m] != -1) return dp[n][m];
-        int up = solve(n-1, m, grid, dp);
-        int left = solve(n, m-1, grid, dp);
-        return dp[n][m] =  up + left;
-    }
+    
 }
