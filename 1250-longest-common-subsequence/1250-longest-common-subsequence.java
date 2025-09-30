7cc9d7369798ad9b9557ca084cf835b2a2e178b6
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-09-30 13:05:37 +0530

    Time: 18 ms (91.53%), Space: 45.5 MB (91.48%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -3,16 +3,18 @@ class Solution {
         int n = s1.length();
         int m = s2.length();
 
-        int[][] dp = new int[n+1][m+1];
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
-        for(int j = 0; j <= m; j++) dp[0][j] = 0;
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
+        
+        for(int j = 0; j <= m; j++) prev[j] = 0;
 
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                if(s1.charAt(i-1) == s2.charAt(j-1)) curr[j] = 1 + prev[j-1];
+                else curr[j] = Math.max(prev[j], curr[j-1]);
             }
+            prev = curr.clone();
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
