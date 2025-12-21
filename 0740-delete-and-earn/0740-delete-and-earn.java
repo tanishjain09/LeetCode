1c5e6adc485d951222ef4e92524fccbb371f9d7a
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-12-21 09:27:25 +0530

    Time: 2 ms (99.85%), Space: 47.3 MB (14.38%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -2,20 +2,15 @@ class Solution {
     public int deleteAndEarn(int[] nums) {
         int max = 0;
         for(int num : nums) max = Math.max(max, num);
-        int[] arr = new int[max+1];
-        for(int num : nums) arr[num]++;
-        int[] dp = new int[max+1];
-        Arrays.fill(dp, -1);
-        return solve(max, arr, dp);
-        
-    }
-    private int solve(int ind, int[] nums, int[] dp){
-        if(ind < 0) return 0;
-        if(nums[ind] == 0) return solve(ind-1, nums, dp);
-        if(dp[ind] != -1) return dp[ind];
-        int pick = 0, notPick = 0;
-        pick = nums[ind] * ind + solve(ind-2, nums, dp);
-        notPick = solve(ind-1, nums, dp);
-        return dp[ind] = Math.max(pick, notPick);
+        int[] earn = new int[max+1];
+        for(int num : nums) earn[num] += num;
+        int prev = earn[0];
+        int prev2 = 0;
+        for(int ind = 1; ind <= max; ind++){
+            int curr = Math.max(prev, prev2+earn[ind]);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 }
