01c42d316885af3baad39916f88934b274e6dacc
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2025-12-29 08:47:36 +0530

    Time: 1481 ms (24.21%), Space: 48.6 MB (31.21%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    HashMap<String, List<Character>> map = new HashMap<>();
+
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+
+        for (String s : allowed) {
+            String pair = s.substring(0, 2);
+            char ch = s.charAt(2);
+            if(!map.containsKey(pair)) map.put(pair, new ArrayList<>());
+            map.get(pair).add(ch);
+        }
+        return dfs(bottom);
+    }
+
+    private boolean dfs(String row) {
+        if (row.length() == 1)
+            return true;
+
+        List<String> nextRow = new ArrayList<>();
+        buildNextRows(row, 0, new StringBuilder(), nextRow);
+
+        for (String next : nextRow) {
+            if (dfs(next))
+                return true;
+        }
+        return false;
+    }
+
+    private void buildNextRows(String row, int idx, StringBuilder sb, List<String> nextRow) {
+        if (idx == row.length()-1) {
+            nextRow.add(sb.toString());
+            return;
+        }
+        String pair = row.substring(idx, idx + 2);
+        if (!map.containsKey(pair))
+            return;
+
+        for (char ch : map.get(pair)) {
+            sb.append(ch);
+            buildNextRows(row, idx + 1, sb, nextRow);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
