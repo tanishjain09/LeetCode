0a71e9f81da184b4b7cad3902a46aeb977e94586
Tanish Sethiya <153806176+tanishjain09@users.noreply.github.com>
2026-01-14 09:50:04 +0530

    Time: 86 ms (98.42%), Space: 149.1 MB (40%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.java
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -1,49 +1,43 @@
 class Solution {
     public double separateSquares(int[][] squares) {
-        double minY = getMin(squares);
-        double maxY = getMax(squares);
-        double precision = 1e-5;
-        while(maxY- minY > precision){
-            double midY = (minY + maxY) / 2;
-            if(isLower(squares, midY)){
-                maxY = midY;
+        double low = Double.MAX_VALUE;
+        double high = -Double.MAX_VALUE;
+        double total = 0.0;
+        for(int[] sq : squares){
+            double y = sq[1];
+            double l = sq[2];
+            total += l * l;
+            low = Math.min(low, y);
+            high = Math.max(high, l + y);
+        }
+
+        double resultY = 0.0;
+        while(high - low > 1e-5){
+            double midY = low + (high - low) / 2.0;
+            resultY = midY;
+            if(check(squares, midY, total)){
+                high = midY;
             }else{
-                minY = midY;
+                low = midY;
             }
         }
-        return minY;
+        return resultY;
     }
-    private double getMin(int[][] squares){
-        double minY = squares[0][1];
-        for(int[] square : squares){
-            minY = Math.min(minY, square[1]);
-        }
-        return minY;
-    }
-    private double getMax(int[][] squares){
-        double maxY = squares[0][1] + squares[0][2];
-        for(int[] square : squares){
-            maxY = Math.max(maxY, (square[1] + square[2]));
-        }
-        return maxY;
-    }
-    boolean isLower(int[][] squares, double midY){
-        double LA = 0, UA = 0;
-        for(int[] square : squares){
-            double bottomY = square[1];
-            double side = square[2];
-            double topY = bottomY + side;
-            if(topY <= midY){
-                LA += side*side;
-            }else if(bottomY >= midY){
-                UA += side * side;
-            }else{
-                double below = midY - bottomY;
-                double above = topY - midY;
-                LA += below * side;
-                UA += above * side;
+    private boolean check(int[][] squares, double midY, double total){
+        double botArea = 0.0;
+        for(int[] sq : squares){
+            double y = sq[1];
+            double l = sq[2];
+
+            double botY = y;
+            double topY = y+l;
+
+            if(midY >= topY){
+                botArea += l*l;
+            }else if(midY >= botY){
+                botArea += (midY - botY) * l;     
             }
         }
-        return LA >= UA;
+        return botArea >= total / 2.0;
     }
 }
